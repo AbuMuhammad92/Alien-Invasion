0afbd3dfe00512af7dd06e352d5c6888f9f83f94
AbuMuhammad <117450920+AbuMuhammad92@users.noreply.github.com>
2023-05-30 16:58:27 +0300

    the ship is moved with the keys left and right.

--- ship.py
+++ ship.py
@@ -12,8 +12,19 @@ class Ship():
         self.image = pygame.image.load('images/ship.bmp')          
         self.rect = self.image.get_rect()          
         # Каждый новый корабль появляется у нижнего края экрана.     
-        self.rect.midbottom = self.screen_rect.midbottom   
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Флаг перемещения         
+        self.moving_right = False
+        self.moving_left = False   
     
+    def update(self):          
+        """Обновляет позицию корабля с учетом флагов."""          
+        if self.moving_right:              
+            self.rect.x += 1
+        if self.moving_left:            
+            self.rect.x -= 1
+
     def blitme(self):          
         """Рисует корабль в текущей позиции."""          
         self.screen.blit(self.image, self.rect)
