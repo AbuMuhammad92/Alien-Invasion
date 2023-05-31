e9b3ea2871f6887f2b42d415caef472d39147ece
AbuMuhammad <117450920+AbuMuhammad92@users.noreply.github.com>
2023-05-31 12:43:36 +0300

    added difficulty increase.

--- alien_invasion.py
+++ alien_invasion.py
@@ -61,7 +61,8 @@ class AlienInvasion:
         """Запускает новую игру при нажатии кнопки Play."""     
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)     
         if button_clicked and not self.stats.game_active:
-            # Сброс игровой статистики.
+            # Сброс игровых настроек.        
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()          
             self.stats.game_active = True
 
@@ -121,7 +122,8 @@ class AlienInvasion:
         if not self.aliens:          
             # Уничтожение существующих снарядов и создание нового флота.         
             self.bullets.empty()          
-            self._create_fleet()                       
+            self._create_fleet()
+            self.settings.increase_speed()                       
 
     def _update_aliens(self):    
         """    
