c7a76692830144728e077f3350b90e103a0d0c3d
AbuMuhammad <117450920+AbuMuhammad92@users.noreply.github.com>
2023-05-31 12:00:46 +0300

    game ready.

--- game_stats.py
+++ game_stats.py
@@ -4,7 +4,10 @@ class GameStats():
     def __init__(self, ai_game):        
         """Инициализирует статистику."""        
         self.settings = ai_game.settings       
-        self.reset_stats()    
+        self.reset_stats()
+
+        # Игра Alien Invasion запускается в активном состоянии.    
+        self.game_active = True    
     
     def reset_stats(self):        
         """Инициализирует статистику, изменяющуюся в ходе игры."""        
