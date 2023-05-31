08051a0ef2c9e7bf22a1fe0e73a96e18c450fc14
AbuMuhammad <117450920+AbuMuhammad92@users.noreply.github.com>
2023-05-31 08:32:42 +0300

    limiting the movement of the ship off the edge of the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,17 +26,26 @@ class AlienInvasion:
         for event in pygame.event.get():                   
             if event.type == pygame.QUIT:                        
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:               
-                if event.key == pygame.K_RIGHT:                      
-                    self.ship.moving_right = True 
-                elif event.key == pygame.K_LEFT:                
-                    self.ship.moving_left = True                    
-            
-            elif event.type == pygame.KEYUP:  
-                if event.key == pygame.K_RIGHT:   
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:                
-                    self.ship.moving_left = False 
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:               
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Реагирует на нажатие клавиш."""
+        if event.key == pygame.K_RIGHT:                      
+            self.ship.moving_right = True 
+        elif event.key == pygame.K_LEFT:                
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:        
+            sys.exit()
+
+    def _check_keyup_events(self, event):    
+        """Реагирует на отпускание клавиш."""            
+        if event.key == pygame.K_RIGHT:   
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:                
+            self.ship.moving_left = False 
     
     def _update_screen(self):   
         """Обновляет изображения на экране и отображает новый экран."""
